e8d8f50093efc167f46274c0736e626bb279040c
nsrichardson <35121234+nsr96@users.noreply.github.com>
2018-05-13 20:37:20 -0400

    Initial commit

---
+++ penguinos.js
@@ -0,0 +1,27 @@
+[
+	{
+		name: "China Penguin",
+		type: "1",
+		penguinimage: {url: "chinapenguin.jpg", "width": 200, "height": 200},
+		desc: "From McMurdo Station in Antarctica, retrieved April 2nd, 2003"
+	},
+	{	
+		name: "Toy Story Penguin, Wheezy",
+		type: "2",
+		penguinimage: {url: "wheezy.png", "width": 200, "height": 200},
+		desc: "This is Wheezy, the penguin from Toy Story the movie. He was brought into the real world via 28 dimensional earth deconstruction."
+
+	},
+	{
+		name: "Club Penguin",
+		type: "3",
+		penguinimage: {url: "clubpenguin.jpg", "width": 200, "height": 200},
+		desc: "This is the penguin from club penguin, retrieved March 2nd, 2013. Turns 24 tomorrow."
+	},
+	{
+		name: "Pizza Penguin",
+		type: "4",
+		penguinimage: {url: "pizzapenguin.jpg", "width": 200, "height": 200},
+		desc: "This penguin loves to eat pizza. Retrieved yesterday."
+	}
+]
